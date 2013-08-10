86eb916ac9dabca39e4f4eeb39e79806afb390f1
Jeff Jia <jeffjia@outlook.com>
2013-08-10 15:49:03 -0700

    finish last update

--- payit_server/payit.py
+++ payit_server/payit.py
@@ -18,8 +18,8 @@ class Seller(db.Model):
 class SellerPayment(db.Model):
   """ Parent: Seller """
   payment = db.StringProperty()
-  key = db.StringProperty()
-  secret = db.StringProperty()
+  app_key = db.StringProperty()
+  app_secret = db.StringProperty()
 
 
 class Buyer(db.Model):
@@ -29,8 +29,8 @@ class Buyer(db.Model):
 class BuyerPayment(db.Model):
   """ Parent: Buyer """
   payment = db.StringProperty()
-  key = db.StringProperty()
-  secret = db.StringProperty()
+  app_key = db.StringProperty()
+  app_secret = db.StringProperty()
 
 
 class Product(db.Model):
@@ -53,14 +53,14 @@ class ProductPullRequestDummyHandler(webapp2.RequestHandler):
   def get(self):
     output = {}
     seller = {
-        'name': 'Demo Bookstore',
+        'merchantName': 'Demo Bookstore',
         'email': 'demo@bookstore.com',
         'sid': 123 }
     output['seller'] = seller
     product_list = []
-    product0 = { 'name': 'book1', 'pid': 0, 'price': 43.0 }
-    product1 = { 'name': 'book2', 'pid': 1, 'price': 63.0 }
-    product2 = { 'name': 'book3', 'pid': 2, 'price': 34.0 }
+    product0 = { 'name': 'book1', 'itemID': 0, 'totalPrice': 43.0, 'itemPrice': 43.0 , 'itemCount': 1 }
+    product1 = { 'name': 'book2', 'itemID': 1, 'totalPrice': 63.5, 'itemPrice': 63.5 , 'itemCount': 1 }
+    product2 = { 'name': 'book3', 'itemID': 2, 'totalPrice': 23.0, 'itemPrice': 23.0 , 'itemCount': 1 }
     product_list.append(product0)
     product_list.append(product1)
     product_list.append(product2)
@@ -72,13 +72,7 @@ class ProductPullRequestHandler(webapp2.RequestHandler):
   """ Buyers pull details of products """
   def get(self):
     """ Sample
-
-        params: ?s-email=12345234&pid=1&pid=2&pid=3
-        returns:
-          [
-            {"price": 43.0, "pid": 0, "name": "book1"},
-            {"price": 63.0, "pid": 1, "name": "book2"},
-            {"price": 34.0, "pid": 2, "name": "book3"}]
+        See dummy request handler
     """
     self.response.write("Hello, Payit!")
 
@@ -86,6 +80,10 @@ class ProductPullRequestHandler(webapp2.RequestHandler):
 class NewProductRequestHandler(webapp2.RequestHandler):
   """ Sellers create a new product """
   def get(self):
+    """ Sample
+
+        params: ?s-email='joe@gmail.com'&pid=1&name='book1'&price=12.0
+    """
     pass
 
 
